9ad7b57e5fef2c31ca65b292eaa574201ac9d8c2
Luka Miletic <lumileti@microsoft.com>
2020-08-22 12:39:49 +0200

    Adding tests which cover invalid initialization parameters.

--- src/test/HangmanTest.java
+++ src/test/HangmanTest.java
@@ -1,9 +1,8 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
-
 import main.Hangman;
 
 class HangmanTest {
@@ -17,6 +16,26 @@ class HangmanTest {
 		
 		assertTrue(game.getWordToGuess().equals(wordToGuess));
 		assertTrue(game.getNumberOfAttempts() == numberOfAttempts);
+	}
+	
+	@Test
+	void testInitialGameStateNonAlphabeticWord() {
+		Exception ex = assertThrows(
+				IllegalArgumentException.class,
+				() -> new Hangman("ЕволуцијаСофтвера", 10)); 
+		
+		assertTrue(ex instanceof IllegalArgumentException);
+		assertTrue(ex.getMessage().equals("Invalid word to guess provided. Only alphabetic words are supported!"));
+	}
+	
+	@Test
+	void testInitialGameStateInvalidNumberOfAttempts() {
+		Exception ex = assertThrows(
+				IllegalArgumentException.class,
+				() -> new Hangman("SoftwareEvolution", -10)); 
+		
+		assertTrue(ex instanceof IllegalArgumentException);
+		assertTrue(ex.getMessage().equals("Invalid number of attempts provided. Number of attempts has to be greater than zero!"));
 	}
 
 }
