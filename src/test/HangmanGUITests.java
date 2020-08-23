351fbe567b6e4f06197233dbe534ba178f7d649b
Luka Miletic <lumileti@microsoft.com>
2020-08-23 14:10:50 +0200

    Adding test for Guess info component initialization.

--- src/test/HangmanGUITests.java
+++ src/test/HangmanGUITests.java
@@ -2,13 +2,13 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.awt.Dimension;
-import java.awt.Point;
-import java.awt.Toolkit;
-
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
 import org.junit.jupiter.api.Test;
+import org.uispec4j.Panel;
+import org.uispec4j.Window;
+
 import main.Constants;
 import main.view.HangmanBoard;
 import main.view.HangmanInputDialogMock;
@@ -27,6 +27,22 @@ class HangmanGUITests {
 		assertTrue(board.getDefaultCloseOperation() == JFrame.EXIT_ON_CLOSE);
 		assertTrue(board.getLocation().equals(new Point(dim.width / 2 - board.getSize().width / 2, dim.height / 2 - board.getSize().height / 2)));
 		assertTrue(board.getWordToGuess().equals(Constants.DIALOG_MOCK_WORD_TO_GUESS.toLowerCase()));
+	}
+	
+	@Test
+	void testAddGuessInfoToHangmanBoard() {
+		HangmanBoard board = new HangmanBoard(new HangmanInputDialogMock());
+		Window window = new Window(board);
+		
+		Panel guessInfo = window.getPanel(Constants.GUESS_INFO_PANEL_NAME);
+		assertTrue(guessInfo.getAwtContainer() instanceof JPanel);
+		assertTrue(guessInfo.getAwtContainer().getComponentCount() == 2);
+		
+		assertTrue(guessInfo.getAwtContainer().getComponent(0).getName().equals(Constants.GUESS_INFO_WORD_LABEL_NAME));
+		assertTrue(guessInfo.getAwtContainer().getComponent(1).getName().equals(Constants.GUESS_INFO_FAILED_ATTEMPTS_LABEL_NAME));
+		
+		assertTrue(((JLabel)guessInfo.getAwtContainer().getComponent(0)).getText().contains(Constants.GUESS_INFO_WORD_LABEL_TEXT));
+		assertTrue(((JLabel)guessInfo.getAwtContainer().getComponent(1)).getText().contains(Constants.GUESS_INFO_FAILED_ATTEMPTS_LABEL_TEXT));
 	}
 	
 }
